280cbf65e49e23d129e6bc01b483be2848516ac8
alexgoni <goni000211@gmail.com>
2024-01-10 22:40:05 +0900

    feat: multiplication table in node.js

---
+++ 구구단/main.js
@@ -0,0 +1,8 @@
+for (let i = 1; i < 10; i++) {
+  for (let j = 1; j < 10; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
+
+// 타이핑 효과
+// https://velog.io/@somangoi/자기소개-페이지-만들기6-자바스크립트-타이핑-효과
