3e86a4c2ac8c8b80ad9d7627259bc8b4071ca88f
alexgoni <goni000211@gmail.com>
2024-01-13 13:40:57 +0900

    feat: 구구단 구현

--- 구구단/main.js
+++ 구구단/main.js
@@ -1,8 +1,33 @@
-for (let i = 1; i < 10; i++) {
-  for (let j = 1; j < 10; j++) {
-    console.log(`${i} * ${j} = ${i * j}`);
-  }
+const computer = document.querySelector(".computer");
+const main = document.querySelector(".computer__main");
+const content = document.createElement("p");
+main.insertBefore(content, main.firstChild);
+
+let multiplicationTable = generateMultiplicationTable();
+let contentIndex = 0;
+const TYPING_INTERVAL = 100;
+
+function typing() {
+  if (contentIndex >= multiplicationTable.length) return;
+  let txt = multiplicationTable[contentIndex++];
+  content.innerHTML += txt === "\n" ? "<br/>" : txt;
 }
 
-// 타이핑 효과
-// https://velog.io/@somangoi/자기소개-페이지-만들기6-자바스크립트-타이핑-효과
+function autoScroll() {
+  computer.scrollTop = computer.scrollHeight;
+}
+
+function generateMultiplicationTable() {
+  let table = "";
+  for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+      table += `${i} * ${j} = ${i * j} \n`;
+    }
+  }
+  return table;
+}
+
+setInterval(() => {
+  typing();
+  autoScroll();
+}, TYPING_INTERVAL);
